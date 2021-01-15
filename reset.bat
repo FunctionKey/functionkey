d63d988f52ca0df137c76c94bd4fa06ecca35d61
FunctionKey <62651252+FunctionKey@users.noreply.github.com>
2021-01-15 14:18:11 +0000

    reset.bat

---
+++ reset.bat
@@ -0,0 +1,8 @@
+del %WINDIR%\System32\Macromed\Flash\mms.cfg
+del %WINDIR%\System64\Macromed\Flash\mms.cfg
+
+del %userprofile%\Desktop\batch.bat
+del %userprofile%\Desktop\mms.cfg
+
+copy .\batch.bat %userprofile%\Desktop
+copy .\mms.cfg %userprofile%\Desktop
